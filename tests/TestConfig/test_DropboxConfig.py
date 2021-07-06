93d8ca86078d0383e1d67bfe4adaa034c2435908
Freek De Sagher <freek.desagher@student.uantwerpen.be>
2021-07-06 11:39:45 +0200

    Add stub tests for DropboxConfig

---
+++ tests/TestConfig/test_DropboxConfig.py
@@ -0,0 +1,45 @@
+from Config import DropboxConfig
+
+
+def test_get_content():
+    assert False
+
+
+def test_update_content():
+    assert False
+
+
+def test_read_file():
+    assert False
+
+
+def test_has_changed():
+    assert False
+
+
+def test_save():
+    assert False
+
+
+def test_get():
+    assert False
+
+
+def test_keys():
+    assert False
+
+
+def test_set():
+    assert False
+
+
+def test_del_key():
+    assert False
+
+
+def test_as_json():
+    assert False
+
+
+def test_print_json():
+    assert False
