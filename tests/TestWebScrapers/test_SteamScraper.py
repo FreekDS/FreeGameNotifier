e2cc0059cd6977a6485045bef087f7536a4460db
Freek De Sagher <freek.desagher@student.uantwerpen.be>
2021-07-06 11:59:27 +0200

    Add stubs for Steam scraper

---
+++ tests/TestWebScrapers/test_SteamScraper.py
@@ -0,0 +1,30 @@
+def test_get_store_url():
+    assert False
+
+
+def test_get_store_name():
+    assert False
+
+
+def test_get_author():
+    assert False
+
+
+def test_get_game_image():
+    assert False
+
+
+def test_get_description():
+    assert False
+
+
+def test_get_store_icon_url():
+    assert False
+
+
+def test_get_title():
+    assert False
+
+
+def test_is_free():
+    assert False
